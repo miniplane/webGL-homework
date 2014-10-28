51c898f05ae44512126aa6b4ce83d52c1c106c8c
miniplane <jasmin.min.wagner@gmail.com>
2014-10-28 14:40:54 +0100

    indices in pyramid, finally working

--- init_buffers.js
+++ init_buffers.js
@@ -16,9 +16,9 @@
 		pyramidVertexPositionBuffer = gl.createBuffer();
 	    gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexPositionBuffer);
 	    var vertices = [
+	    	-1.0, -1.0,  1.0,	// 0
 			 1.0, -1.0,  1.0,	// 1
 			 1.0, -1.0, -1.0,	// 2
-			-1.0, -1.0,  1.0,	// 0
 			-1.0, -1.0, -1.0,	// 3
 	         0.0,  1.0,  0.0,	// 4
 	    ];
@@ -70,7 +70,7 @@
 	    	1, 2, 4,
 	    	2, 3, 4,
 	    	3, 0, 4,
-	    	0, 1, 3, 1, 2, 3,
+	    	0, 3, 1, 1, 3, 2,
 	    ];
 
 	    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(pyramidVertexIndices), gl.STATIC_DRAW);
